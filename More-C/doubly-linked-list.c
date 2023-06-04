7d26a2edc3fa521497fbeca00941eb9cb4abaecd
Stanley Akor <stanleyakor@u.boisestate.edu>
2023-06-03 21:16:41 -0600

    [1] addfirst function

---
+++ More-C/doubly-linked-list.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Dlist
+{
+    int item;
+    int key;
+    struct Dlist *next;
+    struct Dlist *prev;
+}Dlist; 
+
+// setting up global variables!
+Dlist *head = NULL;
+Dlist *tail = NULL;
+
+bool isEmpty()
+{
+    return (head == NULL);
+}
+
+void Forwardprint()
+{
+    Dlist *ptr = head;
+    if(!head)
+        return;
+
+    while(ptr)
+    {
+        printf("%d, %d",ptr->key,ptr->item);
+        ptr = ptr->next;
+        printf(" ");
+    }
+    printf("\n");
+}
+void Backwardprint()
+{
+    Dlist *ptr = tail;
+
+    if(!ptr)
+        return ;
+
+    while(ptr)
+    {
+       printf("%d, %d",ptr->key,ptr->item); 
+       ptr = ptr->prev;
+       printf(" ");
+    }
+    printf("\n");
+}
+
+//**head (the global head)
+ void addfirst(int Item, int Key)
+{
+    Dlist *ptr = (Dlist *)malloc(sizeof(Dlist));
+
+    if(!ptr)
+    {
+        printf("Failed memory allocation!");
+        return;
+    }
+
+    ptr->item = Item;
+    ptr->key = Key;
+
+    //attach to node
+    ptr -> next = head;
+
+    if (head)
+        (head)->prev = ptr;
+
+    //initially empty doublylinked list
+   head = ptr;
+
+}
+
+
+int main(void)
+{
+    // Code for testing the doubly linked list functionality
+    addfirst(100,1);
+    Forwardprint();
+    
+    return 0;
+}

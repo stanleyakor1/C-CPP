80ea635daa610cf798c17c515b8137ff0588ab34
Stanley Akor <stanleyakor@u.boisestate.edu>
2023-06-13 23:48:09 -0600

    fixed

--- More-C/Algorithm/linearsearch.c
+++ More-C/Algorithm/linearsearch.c
@@ -1,4 +1,5 @@
 #include "header.h"
+
 int linsearch(int * arr, int num)
 {
 
@@ -7,7 +8,7 @@ int linsearch(int * arr, int num)
     for(i = 0; i<len; i++)
     {
         if(arr[i] = num)
-            return 1;
+            return i;
     }
 
     return -1;
